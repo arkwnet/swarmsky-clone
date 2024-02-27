e6467f6fd37c3034c1b9aaaa2aca0dc27f50cccf
Sora Arakawa <mail@arkw.net>
2024-02-27 12:59:01 +0900

    Get name and location by scraping

--- checkin.php
+++ checkin.php
@@ -8,25 +8,20 @@ $checkin = json_decode(file_get_contents("https://api.foursquare.com/v2/checkins
 if (isset($checkin["response"]["checkin"]["shares"]["twitter"])) {
   if ($checkin["response"]["checkin"]["shares"]["twitter"] == true) {
     $output = "";
+    $dom = new DOMDocument("1.0", "UTF-8");
+    $html = file_get_contents("https://ja.foursquare.com/v/" . $checkin["response"]["checkin"]["venue"]["id"]);
+    @$dom->loadHTML($html);
+    $xpath = new DOMXpath($dom);
     // スポット名
-    $venue_name = "";
-    if (strpos($checkin["response"]["checkin"]["venue"]["name"], "(") !== false && strpos($checkin["response"]["checkin"]["venue"]["name"], ")") !== false) {
-      $venue_name_split = explode("(", $checkin["response"]["checkin"]["venue"]["name"]);
-      if (preg_match("/[ぁ-ん]+|[ァ-ヴー]+|[一-龠]/u", $venue_name_split[0]) || preg_match("/[0-9]/", $venue_name_split[1])) {
-        $venue_name = $checkin["response"]["checkin"]["venue"]["name"];
-      } else {
-        preg_match("{\((.*)\)}", $checkin["response"]["checkin"]["venue"]["name"], $venue_name_match);
-        $venue_name = $venue_name_match[1];
-      }
-    } else {
-      $venue_name = $checkin["response"]["checkin"]["venue"]["name"];
-    }
+    $venue_name = $xpath->query('//div[@class="venueName"]')->item(0)->nodeValue;
     // スポットの自治体
     $venue_place = "";
-    if (isset($checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1]) && strpos($checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1], "-") === false) {
-      $venue_place = $checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1];
+    $venue_locality = $xpath->query('//span[@itemprop="addressLocality"]')->item(0)->nodeValue;
+    $venue_region = $xpath->query('//span[@itemprop="addressRegion"]')->item(0)->nodeValue;
+    if ($venue_locality != "") {
+      $venue_place = $venue_locality . ", " . $venue_region;
     } else {
-      $venue_place = $checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][0];
+      $venue_place = $venue_region;
     }
     // 投稿文生成
     if (isset($checkin["response"]["checkin"]["shout"])) {
