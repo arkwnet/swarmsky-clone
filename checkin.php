6ac000817a794796603785d67648761d30a2cd77
Sora Arakawa <mail@arkw.net>
2025-09-04 23:49:36 +0900

    Support new format of Push API

--- checkin.php
+++ checkin.php
@@ -4,71 +4,47 @@ if (isset($_POST["checkin"]) == false) {
   exit();
 }
 $push = json_decode($_POST["checkin"], true);
-$checkin = json_decode(file_get_contents("https://api.foursquare.com/v2/checkins/" . $push["id"] . "?oauth_token=" . $FOURSQUARE_ACCESS_TOKEN . "&v=20231010"), true);
-if (isset($checkin["response"]["checkin"]["shares"]["twitter"])) {
-  if ($checkin["response"]["checkin"]["shares"]["twitter"] == true) {
-    $output = "";
-    $dom = new DOMDocument("1.0", "UTF-8");
-    $html = file_get_contents("https://ja.foursquare.com/v/" . $checkin["response"]["checkin"]["venue"]["id"]);
-    @$dom->loadHTML($html);
-    $xpath = new DOMXpath($dom);
-    // スポット名
-    $venue_name = $xpath->query('//div[@class="venueName"]')->item(0)->nodeValue;
-    // スポットの自治体
-    $venue_place = "";
-    $venue_locality = $xpath->query('//span[@itemprop="addressLocality"]')->item(0)->nodeValue;
-    $venue_region = $xpath->query('//span[@itemprop="addressRegion"]')->item(0)->nodeValue;
-    if ($venue_locality != "") {
-      $venue_place = $venue_locality . ", " . $venue_region;
-    } else {
-      $venue_place = $venue_region;
-    }
-    // バックアップシステム
-    if (empty($venue_name) || empty($venue_place)) {
-      // スポット名
-      if (strpos($checkin["response"]["checkin"]["venue"]["name"], "(") !== false && strpos($checkin["response"]["checkin"]["venue"]["name"], ")") !== false) {
-        $venue_name_split = explode("(", $checkin["response"]["checkin"]["venue"]["name"]);
-        if (preg_match("/[ぁ-ん]+|[ァ-ヴー]+|[一-龠]/u", $venue_name_split[0]) || preg_match("/[0-9]/", $venue_name_split[1])) {
-          $venue_name = $checkin["response"]["checkin"]["venue"]["name"];
-        } else {
-          preg_match("{\((.*)\)}", $checkin["response"]["checkin"]["venue"]["name"], $venue_name_match);
-          $venue_name = $venue_name_match[1];
-        }
-      } else {
-        $venue_name = $checkin["response"]["checkin"]["venue"]["name"];
-      }
-      // スポットの自治体
-      if (isset($checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1]) && strpos($checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1], "-") === false) {
-        $venue_place = $checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][1];
-      } else {
-        $venue_place = $checkin["response"]["checkin"]["venue"]["location"]["formattedAddress"][0];
-      }
-    }
-    // 投稿文生成
-    if (isset($checkin["response"]["checkin"]["shout"])) {
-      $output = $checkin["response"]["checkin"]["shout"] . " (@ " . $venue_name . " in " . $venue_place . ") [swarmapp](" . $checkin["response"]["checkin"]["checkinShortUrl"] . ")";
-    } else {
-      $output = "I'm at " . $venue_name . " in ". $venue_place . " [swarmapp](" . $checkin["response"]["checkin"]["checkinShortUrl"] . ")";
-    }
-    // Misskey APIでポスト
-    if ($output != "") {
-      $params = [
-        "i" => $MISSKEY_API_TOKEN,
-        "text" => $output
-      ];
-      $params_json = json_encode($params);
-      $headers = [
-        "Content-Type: application/json",
-        "Accept-Charset: UTF-8",
-      ];
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, "https://" . $MISSKEY_HOST . "/api/notes/create");
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $params_json);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      $result = curl_exec($ch);
-      curl_close($ch);
-    }
+// スポット名と拒否リストを照合
+$venue_name = $push["venue"]["name"];
+for ($i = 0; $i < count($DENY_LIST); $i++) {
+  if($DENY_LIST[$i] != "" && strpos($venue_name, $DENY_LIST[$i]) !== false){
+    exit();
   }
+}
+// スポットの自治体
+$venue_place = "";
+$venue_city = $push["venue"]["location"]["city"];
+$venue_state = $push["venue"]["location"]["state"];
+if ($venue_city != "") {
+  $venue_place = $venue_city . ", " . $venue_state;
+} else {
+  $venue_place = $venue_state;
+}
+// 共有URL (暫定)
+$url = "https://app.foursquare.com/share/checkin/" . $push["id"] . "?s=" . $push["id"] . "&lang=ja";
+// 投稿文生成
+if (isset($push["shout"])) {
+  $output = $push["shout"] . " (@ " . $venue_name . " in " . $venue_place . ") [swarmapp](" . $url . ")";
+} else {
+  $output = "I'm at " . $venue_name . " in ". $venue_place . " [swarmapp](" . $url . ")";
+}
+// Misskey APIでポスト
+if ($output != "") {
+  $params = [
+    "i" => $MISSKEY_API_TOKEN,
+    "text" => $output
+  ];
+  $params_json = json_encode($params);
+  $headers = [
+    "Content-Type: application/json",
+    "Accept-Charset: UTF-8",
+  ];
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://" . $MISSKEY_HOST . "/api/notes/create");
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $params_json);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $result = curl_exec($ch);
+  curl_close($ch);
 }
